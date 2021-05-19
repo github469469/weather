9ea1f02fe0b05cb368393b0876ac504c8e0629cc
sangeeth pm <sangeethpm45@gmail.com>
2021-05-19 17:16:43 +0530

    env removed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Main from './components/Main';
 import './App.css';
 
 function App() {
+
   return (
     <div>
-      <Main></Main>
+      <Main></Main >
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,59 +1,63 @@
 
 import React from 'react'
 import './Main.css'
-import { useState } from 'react'
+import { useState} from 'react'
 // import axios from 'axios'
 const Main = () => {
+    // useEffect(()=>{
+        
+    //     get_weather()
+    // })
 
-    const [val, setVal] = useState('')
-    const [weather, setWeather] = useState('')
-    const [city, setCity] = useState('')
-    const [description, setDescription] = useState('')
-    const [icon, setIcon] = useState('')
-    const data = {
-        key: "ba7e76b12cc767b261a2470526bcb5f7",
-        city: val
-    }
-    const get_weather = async () => {
-        const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${data.key}`)
-        const res = await result.json()
-        setWeather(res.main.temp)
-        //console.log(weather);
-        setCity(res.name)
-        setDescription(res.weather[0].description)
-        setIcon(res.weather[0].icon)
-        return res
-    }
-    const getdetails = (event) => {
-        setVal(event.target.value)
+const [val, setVal] = useState('london')
+const [weather, setWeather] = useState('')
+const [city ,setCity]=useState()
+const [description ,setDescription]=useState('')
+const [icon ,setIcon]=useState('')
+const data={
+    key:process.env.REACT_APP_WEATHER_KEY,
+    city:val
+}
 
-    }
+const get_weather=async ()=>{
+    
+ const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${data.key}`)
+    const res = await result.json()
+    setWeather(res.main.temp)
+    
+    setCity(res.name)
+    setDescription(res.weather[0].description)
+    setIcon(res.weather[0].icon)
+    return res
+}
+// const getdetails=(event)=>{
+//     event.preventDefault();
+//     setVal(event.target.value)
+
+// }
     return (
-        <div>
-            <header>
-                <h2>What's The Weather Like ?</h2>
-            </header>
-            <div className="container">
-
-                <div className="main">
-                    <input type="text" value={val} onChange={getdetails} />
-                    <button className="btn btn-primary" onClick={get_weather}>
-                        Submit
-                    </button >
-
-                </div>
-                <div className="result">
-                    {console.log(weather)}
-                    <h2>{city}</h2>
-                    <h3>
-                    <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" />
-                        {weather}
-                    </h3>
-                    <p>{description}</p>
-
-                </div>
-            </div>
+<div>
+    <header>
+        <h2>What's The Weather Like ?</h2>
+    </header>
+    <div className="container">
+        
+        <div className="main">
+                <input type="text" value={val} onChange={(event)=>setVal(event.target.value) } />
+                <button className="btn btn-primary" onClick={get_weather}>
+                    Submit
+                </button >
+            
         </div>
+       <div className="result">
+           
+          <h2>{city}</h2> 
+           <h3>{weather}</h3>
+           <p>{description}</p>
+           <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="" />
+       </div>
+    </div>
+ </div>
     )
 }
 
